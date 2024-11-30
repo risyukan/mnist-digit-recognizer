08af81ff5ff05968db927b394ca9d96a3068a0b7
Liu <liqiuhan40@gmail.com>
2024-11-30 20:29:35 +0900

    第三章学习第六天

---
+++ deeplearning/Chapter_three/softmax_function.py
@@ -0,0 +1,10 @@
+import numpy as np
+def softmax(a):
+    c = a - np.max(a) #减去行列中最大的数，为了减小exp（x）的大小，防止溢出，使计算正确运行
+    exp_a = np.exp(c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
+a = np.array([1010, 1000, 990])
+print(softmax(a))
